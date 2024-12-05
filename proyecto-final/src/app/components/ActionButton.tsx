1ea30a0634ba086c29e115f0523ca80c1491d159
Gabriel Peraza <sangabo777@gmail.com>
2024-12-05 14:25:38 -0600

    [FIX] No reconoce el color en la clase dinamica

--- proyecto-final/src/app/components/ActionButton.tsx
+++ proyecto-final/src/app/components/ActionButton.tsx
@@ -2,14 +2,20 @@ import React from 'react'
 
 interface ActionButtonProps {
     onClick: () => void;
-    color: string;
+    color: 'green' | 'red' | 'blue';
     disabled?: boolean;
     children: React.ReactNode;
 }
 const ActionButton: React.FC<ActionButtonProps> = ({ onClick, color, disabled = false, children }) => {
+    const baseClasses = 'block py-2 px-10 text-white font-bold rounded';
+    const colorClasses = {
+        green: 'bg-green-500 hover:bg-green-700',
+        red: 'bg-red-500 hover:bg-red-700',
+        blue: 'bg-blue-500 hover:bg-blue-700',
+    };
     return (
         <button
-            className={`block py-2 px-10 bg-${color}-500 text-white font-bold rounded ${disabled ? 'opacity-50' : `hover:bg-${color}-700`}`}
+            className={`${baseClasses} ${colorClasses[color]} ${disabled ? 'opacity-50 cursor-not-allowed' : ''}`}
             onClick={onClick}
 
             disabled={disabled}
