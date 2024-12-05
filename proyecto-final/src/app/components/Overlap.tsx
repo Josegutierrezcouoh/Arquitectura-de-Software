09b09fc05eab463b4d69b7e7bdf3bbb3a034a873
Gabriel Peraza <sangabo777@gmail.com>
2024-12-05 02:21:14 -0600

    fix: overlap layout

--- proyecto-final/src/app/components/Overlap.tsx
+++ proyecto-final/src/app/components/Overlap.tsx
@@ -4,15 +4,20 @@ const Overlap = ({ children, isShowing, onClose }: Readonly<{
     children: React.ReactNode;
     isShowing: boolean;
     onClose: () => void;
-  }>)  => {
-  return (
-    <div className={`${isShowing ? '' : 'hidden'}`}>
-        <div onClick={onClose} className='bg-gray-500 opacity-50 z-50'/>
-        <div className='bg-white p-5 z-51'>
-            {children}
+}>) => {
+    return (
+        <div className={`${isShowing ? '' : 'hidden'}`}>
+            <div
+                onClick={onClose}
+                className='bg-gray-500 w-screen h-screen fixed top-0 left-0 z-10 opacity-50 '
+            />
+            <div
+                className=' bg-white absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 p-5 z-10 rounded'
+            >
+                {children}
+            </div>
         </div>
-    </div>
-        
+
     )
 }
 
