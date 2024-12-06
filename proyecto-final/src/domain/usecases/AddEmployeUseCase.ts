aff58c0a53779a15a294ff451da44244f67a7d18
Gabriel Peraza <sangabo777@gmail.com>
2024-12-06 01:53:20 -0600

    [FTR] Agregando typedoc a addEmployeUseCase

--- proyecto-final/src/domain/usecases/AddEmployeUseCase.ts
+++ proyecto-final/src/domain/usecases/AddEmployeUseCase.ts
@@ -3,14 +3,25 @@ import EmployeesRepositoryProtocol from "../repositories/EmployeesRepositoryProt
 import EmployeesDataSource from "@/infraestructure/datasources/EmployeesDataSource";
 import Employee from "../entities/Employee";
 
+/**
+ * Protocolo para la fuente de datos de empleados.
+ */
 class AddEmployeeUseCase {
     private employeeRepository: EmployeesRepositoryProtocol = new EmployeesRepository(new EmployeesDataSource());
     private _employee: Employee;
 
+    /**
+     * Constructor de la clase de datos de empleados para
+     * @param employee empleado a agregar
+     */
     constructor(employee: Employee) {
         this._employee = employee;
     }
 
+    /**
+    * Agrega un nuevo empleado
+     * @returns una promesa con el nuevo usario agregado
+     */
     async execute(): Promise<Employee> {
         return this.employeeRepository.add(this._employee);
     }
